38bef01780d7c5f9a04136f7b935fdef5206f148
Brian <bri2210433@yahoo.com>
2020-04-14 17:51:02 -0700

    mvp

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const helmet = require("helmet");
+
+const carRouter = require("./cars/car-router");
+
+const server = express();
+
+server.use(helmet());
+server.use(express.json());
+server.use("/", carRouter);
+
+module.exports = server;
